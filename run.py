515c5f60c8043ebe4bd1d2c6a6917e9607517a59
Hansi Zhang <hansi.zhang@ufl.edu>
2017-10-13 18:13:08 -0400

    notification test

--- run.py
+++ run.py
@@ -4,10 +4,7 @@ from riskcalculator import riskcalculator
 from userinfo import userinfo
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
-# import notification
-import logging
-from dbmongo import db
-import config, requests
+import notification
 # import scheduald_email
 
 
@@ -17,28 +14,11 @@ CRCRiskApp.register_blueprint(fb_auth);
 CRCRiskApp.register_blueprint(riskcalculator);
 # CRCRiskApp.register_blueprint(sch_email);
 CRCRiskApp.register_blueprint(userinfo);
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-CRCRiskApp.logger.handlers.extend(gunicorn_error_logger.handlers)
-CRCRiskApp.logger.setLevel(logging.DEBUG)
 
 
-def notify():
-    for user in db.testUser.find():
- # use app access token to send notifications
-        CRCRiskApp.logger.debug('notifications')
-        url = "https://graph.facebook.com/v2.10/" + user['id'] + "/notifications?access_token=" +config.FACEBOOK_APP_ACCESS_TOKEN + "&template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io'"
-        # notification = notfi.put_object(parent_object=user['id'], connection_name='notifications',template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io')
-        proxyDict = {"https" : "https://proxe.shands.ufl.edu:3128"}
-        r = requests.post(url)
-
-@CRCRiskApp.route('/', methods=['GET', 'POST'])
-def homepage():
-    return render_template('index.aspx')
-
+sched = BackgroundScheduler()
+sched.add_job(notification.notify, 'interval', seconds = 10)
+sched.start()
 
 if __name__ == '__main__':
-    CRCRiskApp.logger.debug('start')
-    sched = BackgroundScheduler()
-    sched.add_job(notify, 'interval', seconds = 10)
-    sched.start()
     CRCRiskApp.run(debug = True)
