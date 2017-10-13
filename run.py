df68da25535368834870750ce4c826f2dd968e68
Hansi Zhang <hansi.zhang@ufl.edu>
2017-10-13 17:53:41 -0400

    notification test

--- run.py
+++ run.py
@@ -10,13 +10,6 @@ from dbmongo import db
 import config, requests
 # import scheduald_email
 
-def notify():
-    for user in db.testUser.find():
- # use app access token to send notificaitons
-        url = "https://graph.facebook.com/v2.10/" + user['id'] + "/notifications?access_token=" +config.FACEBOOK_APP_ACCESS_TOKEN + "&template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io'"
-        # notification = notfi.put_object(parent_object=user['id'], connection_name='notifications',template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io')
-        proxyDict = {"https" : "https://proxe.shands.ufl.edu:3128"}
-        r = requests.post(url)
 
 CRCRiskApp = Flask(__name__)
 CRCRiskApp.config.from_object('config')
@@ -27,7 +20,15 @@ CRCRiskApp.register_blueprint(userinfo);
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 CRCRiskApp.logger.handlers.extend(gunicorn_error_logger.handlers)
 CRCRiskApp.logger.setLevel(logging.DEBUG)
-CRCRiskApp.logger.debug('this will show in the log')
+
+def notify():
+    for user in db.testUser.find():
+ # use app access token to send notifications
+        CRCRiskApp.logger.debug('notifications')
+        url = "https://graph.facebook.com/v2.10/" + user['id'] + "/notifications?access_token=" +config.FACEBOOK_APP_ACCESS_TOKEN + "&template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io'"
+        # notification = notfi.put_object(parent_object=user['id'], connection_name='notifications',template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io')
+        proxyDict = {"https" : "https://proxe.shands.ufl.edu:3128"}
+        r = requests.post(url)
 
 @CRCRiskApp.route('/', methods=['GET', 'POST'])
 def homepage():
@@ -38,4 +39,5 @@ if __name__ == '__main__':
     sched = BackgroundScheduler()
     sched.add_job(notify, 'interval', seconds = 10)
     sched.start()
+    CRCRiskApp.logger.debug('start')
     CRCRiskApp.run(debug = True)
