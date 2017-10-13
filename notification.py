3776e80f153c7361d818ac2e98d89fa5fa0fd148
Hansi Zhang <hansi.zhang@ufl.edu>
2017-10-13 14:51:23 -0400

    notification

--- notification.py
+++ notification.py
@@ -11,11 +11,10 @@ def notify():
     # proxy_support = http.ProxyHandler({"https" : "https://proxe.shands.ufl.edu:3128"})
     # opener = http.build_opener(proxy_support, authinfo)
     # http.install_opener(opener)
-    # proxyDict = {"https" : "https://proxe.shands.ufl.edu:3128"}
+    current_app.logger.info('notify')
     for user in db.testUser.find():
  # use app access token to send notificaitons
         url = "https://graph.facebook.com/v2.10/" + user['id'] + "/notifications?access_token=" +config.FACEBOOK_APP_ACCESS_TOKEN + "&template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io'"
         # notification = notfi.put_object(parent_object=user['id'], connection_name='notifications',template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io')
+        proxyDict = {"https" : "https://proxe.shands.ufl.edu:3128"}
         r = requests.post(url, proxies=proxyDict)
-        # r = requests.post(url)
-        current_app.logger.info(r.text)
