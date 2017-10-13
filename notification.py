dbf74ef6a9765d55f9837d7fdc2083597f648a3b
Hansi Zhang <hansi.zhang@ufl.edu>
2017-10-13 14:43:49 -0400

    notification

--- notification.py
+++ notification.py
@@ -3,6 +3,7 @@ import config
 import facebook as fb
 from dbmongo import db
 import requests
+from flask import current_app
 
 def notify():
     # notfi = fb.GraphAPI(config.FACEBOOK_APP_ACCESS_TOKEN)
@@ -15,6 +16,6 @@ def notify():
  # use app access token to send notificaitons
         url = "https://graph.facebook.com/v2.10/" + user['id'] + "/notifications?access_token=" +config.FACEBOOK_APP_ACCESS_TOKEN + "&template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io'"
         # notification = notfi.put_object(parent_object=user['id'], connection_name='notifications',template='Please check your recent CRC cancer risk test result: http://mycancerrisk.io')
-        # r = requests.post(url, proxies=proxyDict)
-        r = requests.post(url)
-        print(r.text)
+        r = requests.post(url, proxies=proxyDict)
+        # r = requests.post(url)
+        current_app.logger.info(r.text)
