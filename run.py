af70f08b55718661e74d6bdc3af800f60ff37332
Hansi Zhang <hansi.zhang@ufl.edu>
2017-11-08 22:05:15 -0500

    cache contorl

--- run.py
+++ run.py
@@ -19,6 +19,12 @@ CRCRiskApp.register_blueprint(userinfo);
 def homepage():
     return render_template('index.aspx')
 
+@app.after_request
+def add_header(response):
+    response.cache_control.public = True
+    response.cache_control.max_age = 300
+    return response
+
 sched = BackgroundScheduler()
 sched.add_job(notification.notify, 'interval', seconds = 604800)
 sched.start()
